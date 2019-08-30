3c18a9ee7284682c1835a11a8eb6fdee35c03f5c
Matt Wade <matt.wade@designory.com>
2019-08-30 18:43:09 -0500

    Returns all arrays, including duplicate ones
    Time will tell whether this is a useful API or not, but probably isn't
    so bad that I duplicate the data I'm sending back.

--- src/gen-diff-object.js
+++ src/gen-diff-object.js
@@ -116,8 +116,20 @@ const diffDirectories = async (build_old, build_new, { blacklist = CHANGED_FILES
 	files_changed = files_changed.filter(file => !CHANGED_FILES_BLACKLIST.includes(file));
 
 	// Sort all files alphabetically
-	files_deleted.sort();
+	files_added.sort();
+	files_updated.sort();
 	files_changed.sort();
+	files_deleted.sort();
 
 	log && console.log('Done'.green);
+
+	return {
+		filesAdded: files_added,
+		filesUpdated: files_updated,
+
+		// Combination of 'Added' and 'Updated
+		filesChanged: files_updated,
+
+		filesDeleted: files_deleted,
+	};
 };

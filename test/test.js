d1e921d3dec59bafa931732de9850bf3a0582512
Matt Wade <matt.wade@designory.com>
2019-08-30 20:17:55 -0500

    Tests that method returns three keys

--- test/test.js
+++ test/test.js
@@ -10,5 +10,15 @@ if (process.platform !== 'darwin') {
 const PATHS_LOOKUP = require('./folders-to-compare/paths-lookup');
 
 describe('`diffDirectories` method', function() {
+	it('should return three keys: `filesAdded`, `filesDeleted`, and `filesUpdated`', function(done) {
+		let { old_dir, new_dir } = PATHS_LOOKUP['no-change'];
 
+		diffDirectories(old_dir, new_dir, { log: false }).then(result => {
+			let keys = Object.keys(result);
+			keys.sort();
+
+			assert.deepEqual(keys, ['filesAdded', 'filesDeleted', 'filesUpdated']);
+			done();
+		});
+	});
 });

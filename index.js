a005c34694c14ba0e9637ca6c428d4eca2680b4f
Matt Wade <matt.wade@designory.com>
2019-08-30 19:22:00 -0500

    Uses export for main diffing logic
    This is abstracted out into a separate function to aid with automated
    tests (to be made in the future).

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const colors = require('colors');
 const glob = require('globby');
 const { trimStart } = require('lodash');
 
+const diffDirectories = require('./src/gen-diff-object');
+
 (async () => {
 	// @todo use an argument parsing lib
 	let build_old = process.argv[2];
@@ -36,105 +38,10 @@ const { trimStart } = require('lodash');
 
 	// If we are here, both directories exist, so let's compute the differences between them
 
-	let diff_result;
-	const CHANGED_FILES_BLACKLIST = [
-		'.DS_Store'
-	];
-	try {
-		process.stdout.write('Diffing directories... '.yellow);
-		diff_result = await execPromise(`diff -q -r "${build_old}" "${build_new}"`);
-	} catch (error) {
-		/**
-		 * `diff` exits with code '1' (an error) when it finds differences. But this is actually what we want!
-		 * If we have something other than 1, then quit our program. Otherwise, get our stdout and carry on.
-		 */
-		if (error.code === 1) {
-			diff_result = { stdout: error.stdout };
-		} else {
-			console.log('\nThere was an error running the "diff" process:\n'.red, error);
-			process.exit(1);
-		}
-	}
-
-	console.log('Done'.green);
-
-	process.stdout.write('Parsing diff results... '.yellow);
-
-	/**
-	 * @note Documentation notes on `diff` program output:
-	 *
-	 * We are passing in the `-q` and `-r` options, which means we only output
-	 * the file names that are different, and we recursively look through the directories
-	 * for any differences (respectively).
-	 *
-	 * There are 3 variations on what the output could be:
-	 *
-	 * 1. New files
-	 * 2. Deleted files
-	 * 3. Updated
-	 *
-	 * Four our builds (under S3), we no longer differentiate between _updated_ and _new_
-	 * files, so we only care about those that are _deleted_, and those that have _changed_.
-	 *
-	 * Note that in the below examples, `build-new` and `build-old` will be replaced by whatever
-	 * arguments you passed into the your `build_new` and `build_old` variables / arguments above.
-	 *
-	 * If a file is **new**, its output will look like
-	 *
-	 *     Only in build-new: a-newly-added-file.txt
-	 *     Only in build-new/existing-dir: a-newly-added-file-within-a-directory.txt
-	 *     Only in build-new: a-newly-added-directory
-	 *
-	 * If a file is **deleted**, its output will look like
-	 *
-	 *     Only in build-old: a-deleted-file.txt
-	 *     Only in build-old/existing-dir: a-deleted-file-within-a-directory.txt
-	 *     Only in build-old: a-deleted-directory
-	 *
-	 * And if a file has been **updated**, its output will look like
-	 *
-	 *     Files build-old/file.txt and build-new/file.txt differ
-	 *     Files build-old/existing-dir/other-file.txt and build-new/existing-dir/other-file.txt differ
-	 */
-	let diff_output = diff_result.stdout;
-	let diff_output_array = diff_output.split('\n').filter(v => v);
-
-	const files_deleted_regex = new RegExp(`^Only in ${build_old}(.*?): (.*)$`, 'i');
-	const files_added_regex = new RegExp(`^Only in ${build_new}(.*?): (.*)$`, 'i');
-	const files_updated_regex = new RegExp(`^Files (.*?) and (.*?) differ$`, 'i');
-
-	// Calculate what files were deleted, added, or updated
-	let files_deleted = diff_output_array.filter(line => files_deleted_regex.test(line));
-	let files_added = diff_output_array.filter(line => files_added_regex.test(line));
-	let files_updated = diff_output_array.filter(line => files_updated_regex.test(line));
-
-	// Parse out real file paths from our above lists
-	files_deleted = files_deleted.map(line => {
-		let [_match, subdir, file] = files_deleted_regex.exec(line);
-
-		// Trim leading slash on subdir
-		subdir = trimStart(subdir, path.sep);
-
-		return subdir ? subdir + path.sep + file : file;
-	});
-	files_added = files_added.map(line => {
-		let [_match, subdir, file] = files_added_regex.exec(line);
-
-		// Trim leading slash on subdir
-		subdir = trimStart(subdir, path.sep);
-
-		return subdir ? subdir + path.sep + file : file;
-	});
-	files_updated = files_updated.map(line => {
-		// `old_path` and `new_path` only differ in their leading directory string. Otherwise, the file pathname is the same.
-		let [_match, old_path] = files_updated_regex.exec(line);
-
-		// Remove build dir from `old_path` and `new_path`, and trim leading slash
-		let file_path = old_path.replace(new RegExp(`^${build_old}`), '');
-		file_path = trimStart(file_path, path.sep);
-
-		return file_path;
-	});
+	let { filesAdded: files_added, filesUpdated: files_updated, filesDeleted: files_deleted } = await diffDirectories(
+		build_old,
+		build_new
+	);
 
 	if (files_added.length + files_updated + files_deleted.length === 0) {
 		console.error(`No files were different between "${build_old}" and "${build_new}", exiting`);
@@ -144,14 +51,8 @@ const { trimStart } = require('lodash');
 	// Concat 'added' and 'updated' since functionally they are no different when uploading to AWS (or other)
 	let files_changed = files_updated.concat(files_added);
 
-	// Filter files we don't care about
-	files_changed = files_changed.filter(file => !CHANGED_FILES_BLACKLIST.includes(file));
-
-	// Sort all files alphabetically
-	files_deleted.sort();
+	// Sort newly formed `files_changed`; other lists are already sorted
 	files_changed.sort();
-
-	console.log('Done'.green);
 
 	let output_dir = 'build_for_upload';
 	output_dir = path.resolve(output_dir);
@@ -194,6 +95,5 @@ const { trimStart } = require('lodash');
 		console.log('  ' + files_changed.join('\n  ') + '\n');
 	}
 
-	
 	console.log(`All changed files have been copied to ${relative_output_dir.cyan}, and zipped in ${(relative_output_dir + '.zip').cyan}\n`);
 })();

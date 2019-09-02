9bb51d47b00f1d901ef6cdd2784c71ea3c6af525
Matt Wade <romellem@users.noreply.github.com>
2019-09-01 23:34:01 -0500

    Comments out macOS test-check
    Testing on CircleCI / Linux / Docker enviro

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@ const diffDirectories = require('../src/gen-diff-object');
 const DEFAULT_BLACKLIST = require('../src/diff-blacklist');
 
 // Ensure tests are run on mac operating system
-if (process.platform !== 'darwin') {
-	console.error('Tests are only supported on macOS at this time');
-	process.exit(1);
-}
+// if (process.platform !== 'darwin') {
+// 	console.error('Tests are only supported on macOS at this time');
+// 	process.exit(1);
+// }
 
 const PATHS_LOOKUP = require('./folders-to-compare/paths-lookup');
 

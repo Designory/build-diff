cc082076741ca354f47f009b9f9e4ce6d6d67a7c
Matt Wade <matt.wade@designory.com>
2020-07-09 12:24:40 -0500

    Prettier

--- test/test.js
+++ test/test.js
@@ -15,12 +15,12 @@ const FILES_DELETED = 'filesDeleted';
 const FILES_UPDATED = 'filesUpdated';
 const RESULTS_KEYS = [FILES_ADDED, FILES_DELETED, FILES_UPDATED];
 
-describe('`diffDirectories` method', function() {
-	it('should return three keys: `filesAdded`, `filesDeleted`, and `filesUpdated`', function(done) {
+describe('`diffDirectories` method', function () {
+	it('should return three keys: `filesAdded`, `filesDeleted`, and `filesUpdated`', function (done) {
 		let { old_dir, new_dir } = PATHS_LOOKUP['no-change'];
 
 		diffDirectories(old_dir, new_dir, { quiet: true })
-			.then(result => {
+			.then((result) => {
 				let keys = Object.keys(result);
 				keys.sort();
 
@@ -30,12 +30,12 @@ describe('`diffDirectories` method', function() {
 			.catch(done);
 	});
 
-	it('should return empty arrays when no differences are found', function(done) {
+	it('should return empty arrays when no differences are found', function (done) {
 		let { old_dir, new_dir } = PATHS_LOOKUP['no-change'];
 
 		diffDirectories(old_dir, new_dir, { quiet: true })
-			.then(result => {
-				RESULTS_KEYS.forEach(key => {
+			.then((result) => {
+				RESULTS_KEYS.forEach((key) => {
 					assert.strictEqual(result[key].length, 0);
 				});
 
@@ -44,11 +44,11 @@ describe('`diffDirectories` method', function() {
 			.catch(done);
 	});
 
-	it('should find new files', function(done) {
+	it('should find new files', function (done) {
 		let { old_dir, new_dir } = PATHS_LOOKUP['new-file'];
 
 		diffDirectories(old_dir, new_dir, { quiet: true })
-			.then(result => {
+			.then((result) => {
 				assert.strictEqual(result[FILES_ADDED].length, 1);
 				assert.strictEqual(result[FILES_DELETED].length, 0);
 				assert.strictEqual(result[FILES_UPDATED].length, 0);
@@ -58,11 +58,11 @@ describe('`diffDirectories` method', function() {
 			.catch(done);
 	});
 
-	it('should find new folders', function(done) {
+	it('should find new folders', function (done) {
 		let { old_dir, new_dir } = PATHS_LOOKUP['new-folder'];
 
 		diffDirectories(old_dir, new_dir, { quiet: true })
-			.then(result => {
+			.then((result) => {
 				assert.strictEqual(result[FILES_ADDED].length, 1);
 				assert.strictEqual(result[FILES_DELETED].length, 0);
 				assert.strictEqual(result[FILES_UPDATED].length, 0);
@@ -73,11 +73,11 @@ describe('`diffDirectories` method', function() {
 	});
 
 	// @todo Remove this test once we _do_ return all files within new folders
-	it('should find new folders but not report all new files within that new folder', function(done) {
+	it('should find new folders but not report all new files within that new folder', function (done) {
 		let { old_dir, new_dir } = PATHS_LOOKUP['new-folder-with-many-new-files'];
 
 		diffDirectories(old_dir, new_dir, { quiet: true })
-			.then(result => {
+			.then((result) => {
 				// New folder contains 3 files, but just gets reported as one new folder
 				assert.strictEqual(result[FILES_ADDED].length, 1);
 				assert.strictEqual(result[FILES_DELETED].length, 0);
@@ -88,11 +88,11 @@ describe('`diffDirectories` method', function() {
 			.catch(done);
 	});
 
-	it('should find updated files', function(done) {
+	it('should find updated files', function (done) {
 		let { old_dir, new_dir } = PATHS_LOOKUP['updated-file'];
 
 		diffDirectories(old_dir, new_dir, { quiet: true })
-			.then(result => {
+			.then((result) => {
 				assert.strictEqual(result[FILES_ADDED].length, 0);
 				assert.strictEqual(result[FILES_DELETED].length, 0);
 				assert.strictEqual(result[FILES_UPDATED].length, 1);
@@ -102,11 +102,11 @@ describe('`diffDirectories` method', function() {
 			.catch(done);
 	});
 
-	it('should find deleted files', function(done) {
+	it('should find deleted files', function (done) {
 		let { old_dir, new_dir } = PATHS_LOOKUP['deleted-file'];
 
 		diffDirectories(old_dir, new_dir, { quiet: true })
-			.then(result => {
+			.then((result) => {
 				assert.strictEqual(result[FILES_ADDED].length, 0);
 				assert.strictEqual(result[FILES_DELETED].length, 1);
 				assert.strictEqual(result[FILES_UPDATED].length, 0);
@@ -116,11 +116,11 @@ describe('`diffDirectories` method', function() {
 			.catch(done);
 	});
 
-	it('should handle new/updated/deleted all at once', function(done) {
+	it('should handle new/updated/deleted all at once', function (done) {
 		let { old_dir, new_dir } = PATHS_LOOKUP['all-combinations'];
 
 		diffDirectories(old_dir, new_dir, { quiet: true })
-			.then(result => {
+			.then((result) => {
 				// 1 new file + 1 new folder
 				assert.strictEqual(result[FILES_ADDED].length, 2);
 				assert.strictEqual(result[FILES_DELETED].length, 1);
@@ -132,20 +132,23 @@ describe('`diffDirectories` method', function() {
 	});
 
 	// @todo support regex/globs instead of exact matches
-	it('should ignore updated files on blacklist', function(done) {
+	it('should ignore updated files on blacklist', function (done) {
 		let { old_dir, new_dir } = PATHS_LOOKUP['blacklist-file'];
 
 		diffDirectories(old_dir, new_dir, { quiet: true })
-			.then(result => {
+			.then((result) => {
 				assert.strictEqual(result[FILES_ADDED].length, 0);
 				assert.strictEqual(result[FILES_DELETED].length, 0);
 				assert.strictEqual(result[FILES_UPDATED].length, 1);
 			})
 			.then(() =>
 				// Run diff again, but ignore 'file.txt'
-				diffDirectories(old_dir, new_dir, { quiet: true, blacklist: [...DEFAULT_BLACKLIST, 'file.txt'] })
+				diffDirectories(old_dir, new_dir, {
+					quiet: true,
+					blacklist: [...DEFAULT_BLACKLIST, 'file.txt'],
+				})
 			)
-			.then(result => {
+			.then((result) => {
 				// With blacklist, we shouldn't have any diffs
 				assert.strictEqual(result[FILES_ADDED].length, 0);
 				assert.strictEqual(result[FILES_DELETED].length, 0);
